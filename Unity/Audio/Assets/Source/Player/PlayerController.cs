b9ca4252e390b860086f035290bdccc82abde79a
MiguelEsat <valeroce@esat-alumni.com>
2025-12-07 12:19:40 +0100

    Added jump mechanic

--- Unity/Audio/Assets/Source/Player/PlayerController.cs
+++ Unity/Audio/Assets/Source/Player/PlayerController.cs
@@ -1,30 +1,43 @@
 using FMODUnity;
 using UnityEngine;
-using UnityEngine.InputSystem.XR;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Configuration")] public float speed = 3f;
+    [Header("Configuración Movimiento")]
+    public float speed = 5f;
     public float sensitivity = 200f;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+
+    [Header("Referencias")]
     public new Transform camera;
 
-    [Header("Conf FMOD")]
+    [Header("Configuración FMOD")]
     public EventReference sound;
-    public float interval = 0.75f;
+    public float interval = 0.5f;
 
     private float _rotationX = 0f;
     private CharacterController _controller;
     private float timerfootsteps = 0f;
 
+    private Vector3 _velocity;
+    private float _groundedTimer; 
+
     void Start()
     {
         _controller = GetComponent<CharacterController>();
-
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
+    {
+        HandleLook();
+        HandleMovement();
+    }
+
+    void HandleLook()
     {
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
@@ -33,26 +46,47 @@ public class PlayerController : MonoBehaviour
         _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
 
         camera.localRotation = Quaternion.Euler(_rotationX, 0f, 0f);
-
         transform.Rotate(Vector3.up * mouseX);
+    }
 
+    void HandleMovement()
+    {
+        if (_controller.isGrounded)
+        {
+            _groundedTimer = 0.2f; 
+
+            if (_velocity.y < 0)
+            {
+                _velocity.y = -2f;
+            }
+        }
+        else
+        {
+            _groundedTimer -= Time.deltaTime;
+        }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 mover = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z;
+        _controller.Move(move * (speed * Time.deltaTime));
 
-        _controller.Move(mover * (speed * Time.deltaTime));
+        if (Input.GetButtonDown("Jump") && _groundedTimer > 0)
+        {
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 
-        _controller.Move(Physics.gravity * Time.deltaTime);
+            _groundedTimer = 0;
+        }
 
-        FootSteps(mover);
+        _velocity.y += gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+
+        FootSteps(move);
     }
 
     void FootSteps(Vector3 vectorMovimiento)
     {
-
-        if (_controller.isGrounded && vectorMovimiento.magnitude > 0)
+        if (_controller.isGrounded && vectorMovimiento.magnitude > 0.1f)
         {
             timerfootsteps -= Time.deltaTime;
 
@@ -70,11 +104,9 @@ public class PlayerController : MonoBehaviour
 
     void PlaySound()
     {
-
         if (!sound.IsNull)
         {
             RuntimeManager.PlayOneShot(sound, transform.position);
         }
     }
-
 }
