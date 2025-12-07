f16798feaed43722b32634edcb1b50eeee949608
FerruMG45 <ferrufinome@esat-alumni.com>
2025-12-07 17:11:26 +0100

    Added more ambience sounds for immersion

---
+++ Unity/Audio/Assets/Source/LoopableAmbiance.cs
@@ -0,0 +1,51 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
+
+public class LoopableAmbiance : MonoBehaviour
+{
+    [Header("FMOD")] public EventReference soundEvent;
+    
+    private EventInstance _soundInstance;
+
+    private bool _isPlaying = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _soundInstance = RuntimeManager.CreateInstance(soundEvent);
+        StartAmbiance(gameObject.transform, gameObject.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void StartAmbiance(Transform source, Vector3 worldPos)
+    {
+        if (_isPlaying) return;
+
+        if (source)
+        {
+            RuntimeManager.AttachInstanceToGameObject(_soundInstance, source, source.GetComponent<Rigidbody>());
+        }
+        else
+        {
+            _soundInstance.set3DAttributes(RuntimeUtils.To3DAttributes(worldPos));
+        }
+
+        _soundInstance.start();
+        _isPlaying = true;
+    }
+
+    public void StopAmbience(bool fadeOut = true)
+    {
+        if (!_isPlaying) return;
+
+        _soundInstance.stop(fadeOut ? STOP_MODE.ALLOWFADEOUT : STOP_MODE.IMMEDIATE);
+        _soundInstance.release();
+        _isPlaying = false;
+    }
+}
---
+++ Unity/Audio/Assets/Source/OneShotEmitter.cs
@@ -0,0 +1,49 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using System.Collections;
+
+
+public class OneShotEmitter : MonoBehaviour
+{
+    [Header("FMOD")] public EventReference soundEvent;
+    [Header("FMOD Settings")]
+    
+    private EventInstance _soundInstance;
+
+    public Vector2 timeRange = new Vector2(3.0f, 7.0f);
+    
+    public float minPitch = 0.95f;
+    public float maxPitch = 1.05f;
+    
+    
+    void Start()
+    {
+        StartCoroutine(PlayRoutine());
+
+    }
+
+    private IEnumerator PlayRoutine()
+    {
+        while (true)
+        {
+            float delay = Random.Range(timeRange.x, timeRange.y);
+            yield return new WaitForSeconds(delay);
+
+            PlayOneShot3D();
+        }
+    }
+    
+    private void PlayOneShot3D()
+    {
+        _soundInstance =  RuntimeManager.CreateInstance(soundEvent);
+        
+        float randomPitch = Random.Range(minPitch, maxPitch);
+        _soundInstance.setPitch(randomPitch);
+        
+        _soundInstance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject));
+        _soundInstance.start();
+        _soundInstance.release();
+    }
+ }
+
